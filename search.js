694e34222edef0d6700ef171838c2bfeb35e2977
wdyjwdy <xxccat@gmail.com>
2025-09-09 12:07:12 +0800

    update

--- search.js
+++ search.js
@@ -1,10 +1,9 @@
 const search = document.querySelector("#search");
-const result = document.querySelector("#result");
+const result = document.querySelector(".result");
 search.addEventListener("keydown", function (event) {
   if (event.code === "Enter") {
     result.innerHTML = "";
     const query = this.value;
-    console.log(data);
     data
       .filter((d) => d.title.includes(query))
       .forEach((x) => {
