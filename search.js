3e0315bac2e8ca82aacec33d55bfc585c56998b9
wdyjwdy <xxccat@gmail.com>
2025-09-10 00:31:56 +0800

    update

--- search.js
+++ search.js
@@ -5,8 +5,16 @@ search.addEventListener("keydown", function (event) {
     result.innerHTML = "";
     const query = this.value;
     data
-      .filter((d) => d.title.includes(query))
-      .forEach((x) => {
+      .filter((d) => d.title && d.title.includes(query))
+      .map((x) => ({ ...x, catagory: x.url.split("/")[1] }))
+      .sort((a, b) => (a.catagory < b.catagory ? 1 : -1))
+      .forEach((x, i, a) => {
+        const prevCatagory = a[i - 1]?.catagory;
+        if (x.catagory !== prevCatagory) {
+          const p = document.createElement("p");
+          p.textContent = x.catagory;
+          result.appendChild(p);
+        }
         const link = document.createElement("a");
         link.href = "." + x.url;
         link.textContent = x.title;
